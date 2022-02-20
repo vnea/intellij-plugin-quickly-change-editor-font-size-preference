311f6e5c0146bfa756885ed919826953378093fa
Victor Nea <vnea@octo.com>
2022-02-20 03:28:25 +0100

    feat: Add protection to constructor

--- src/main/kotlin/com/vnea/intellij/plugin/quicklychangeeditorfontsizepreference/model/ChangeEditorFontSize.kt
+++ src/main/kotlin/com/vnea/intellij/plugin/quicklychangeeditorfontsizepreference/model/ChangeEditorFontSize.kt
@@ -1,7 +1,17 @@
 package com.vnea.intellij.plugin.quicklychangeeditorfontsizepreference.model
 
-class ChangeEditorFontSize(fontSize: Int) {
+class ChangeEditorFontSize(min: Int = 4, max: Int = 40, fontSize: Int) {
 
-    var fontSize = fontSize
+    var fontSize: Int
         private set
+
+    init {
+        if (fontSize < min) {
+            this.fontSize = min
+        } else if (fontSize > max) {
+            this.fontSize = max
+        } else {
+            this.fontSize = fontSize
+        }
+    }
 }
--- src/test/kotlin/com/vnea/intellij/plugin/quicklychangeeditorfontsizepreference/model/ChangeEditorFontSizeUTest.kt
+++ src/test/kotlin/com/vnea/intellij/plugin/quicklychangeeditorfontsizepreference/model/ChangeEditorFontSizeUTest.kt
@@ -6,14 +6,40 @@ import org.junit.jupiter.api.Test
 internal class ChangeEditorFontSizeUTest {
 
     @Test
-    fun `fontSize SHOULD return given font size`() {
+    fun `constructor SHOULD set font size`() {
         // Given
-        val changeEditorFontSize = ChangeEditorFontSize(10)
+        val fontSize = 10
 
         // When
-        val fontSizeReturned = changeEditorFontSize.fontSize
+        val changeEditorFontSize = ChangeEditorFontSize(fontSize = fontSize)
 
         // Then
-        assertEquals(10, fontSizeReturned)
+        assertEquals(fontSize, changeEditorFontSize.fontSize)
+    }
+
+    @Test
+    fun `constructor SHOULD set font size to min WHEN given font size is inferior to min`() {
+        // Given
+        val min = 4
+        val fontSize = 3
+
+        // When
+        val changeEditorFontSize = ChangeEditorFontSize(min = min, fontSize = fontSize)
+
+        // Then
+        assertEquals(min, changeEditorFontSize.fontSize)
+    }
+
+    @Test
+    fun `constructor SHOULD set font size to max WHEN given font size is superior to max`() {
+        // Given
+        val max = 40
+        val fontSize = 41
+
+        // When
+        val changeEditorFontSize = ChangeEditorFontSize(max = max, fontSize = fontSize)
+
+        // Then
+        assertEquals(max, changeEditorFontSize.fontSize)
     }
 }
